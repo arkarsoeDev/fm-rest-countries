47b01c5be7e6130c4c46ded9c4e32a912bd88152
Arkar <arkar.fr@gmail.com>
2022-04-04 22:53:13 +0630

    Add styles

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
-  <header></header>
+  <header>
+    <Nav></Nav>
+  </header>
   <router-view />
 </template>
 <script>
+import Nav from "@/components/Nav";
 export default {
   name: "App",
+  components: {
+    Nav,
+  },
 };
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+@import "@/assets/app/scss/main";
+</style>
