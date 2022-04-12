47a7c0b220ad4c7db77be349ab74f8bc0029952a
Arkar <arkar.fr@gmail.com>
2022-04-12 21:40:43 +0630

    Add footer

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,17 @@
       <Nav></Nav>
     </header>
     <router-view />
+    <Footer></Footer>
   </div>
 </template>
 <script>
 import Nav from "@/components/Nav";
+import Footer from "@/components/Footer";
 export default {
   name: "App",
   components: {
     Nav,
+    Footer,
   },
   computed: {
     themeDark() {
